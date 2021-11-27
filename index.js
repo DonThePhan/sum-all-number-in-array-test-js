70aa51577cef3694a825e5fec9adaedc471be000
DonThePhan <donthephan@gmail.com>
2021-11-27 14:03:31 -0500

    add test case & commets

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function sumItems(array) {
 	let sum = 0;
 
-	for (let num of array) {
-		if (Array.isArray(num)) sum += sumItems(num);
-		else sum += num;
+  for (let num of array) {
+		if (Array.isArray(num)) sum += sumItems(num); // recursive case
+    else sum += num; // base case
 	}
 	return sum;
 }
+
+// console.log(sumItems([[1, 2, [[3], 4]], 5, []]))
 
 module.exports = sumItems;
